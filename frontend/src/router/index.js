673b1d77e5ace95f658c06bb170b1fc640340755
Nyanyagulugulu <qianxun114514@gmail.com>
2025-12-30 03:20:54 +0800

    添加管理员页面

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import SearchResultsView from '@/views/SearchResultsView.vue'
+import AdminView from '@/views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'search-query',
       component: SearchResultsView,
       props: true
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView
     }
   ]
 })
