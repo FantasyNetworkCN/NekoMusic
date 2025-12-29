5da9c749977946f3e85c3af9945bc3eeb7db037c
Nyanyagulugulu <qianxun114514@gmail.com>
2025-12-30 01:48:43 +0800

    修复错误

--- backend/src/main/java/com/neko/music/Main.java
+++ backend/src/main/java/com/neko/music/Main.java
@@ -6,6 +6,10 @@ import com.neko.music.handlers.MusicSearchHandler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import jakarta.servlet.DispatcherType;
+import java.util.EnumSet;
 
 public class Main {
     private static Server server;
@@ -31,6 +35,9 @@ public class Main {
         context.setContextPath("/");
         server.setHandler(context);
         
+        // 添加CORS过滤器
+        addCorsFilter(context);
+        
         // 注册搜索音乐API处理器
         ServletHolder holder = new ServletHolder(new MusicSearchHandler());
         context.addServlet(holder, "/api/music/search");
@@ -41,6 +48,14 @@ public class Main {
         System.out.println("API端点:");
         System.out.println("  POST /api/music/search - 搜索音乐");
         server.join();
+    }
+    
+    private static void addCorsFilter(ServletContextHandler context) {
+        org.eclipse.jetty.servlet.FilterHolder cors = context.addFilter(CrossOriginFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
     }
     
     public static DatabaseManager getDatabaseManager() {
