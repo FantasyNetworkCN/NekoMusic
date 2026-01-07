81028ebd6c5842f5020b024a8f8033ae6d1f6794
Nyanyagulugulu <qianxun114514@gmail.com>
2026-01-08 00:38:12 +0800

    自定义启动页面

--- Android/app/src/main/java/com/neko/music/ui/theme/Color.kt
+++ Android/app/src/main/java/com/neko/music/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.neko.music.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// 网易云音乐风格红色系
+val NetEaseRed = Color(0xFFC20C0C) // 网易云音乐标志性红色
+val NetEaseRedDark = Color(0xFF8B0000) // 深红色
+val NetEaseRedLight = Color(0xFFE63946) // 浅红色
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// 浅色主题
+val PrimaryLight = NetEaseRed
+val SecondaryLight = Color(0xFFEC4141)
+val TertiaryLight = Color(0xFFFF6B6B)
+val BackgroundLight = Color(0xFFFFFFFF)
+val SurfaceLight = Color(0xFFF7F7F7)
+
+// 深色主题
+val PrimaryDark = NetEaseRed
+val SecondaryDark = Color(0xFFD32F2F)
+val TertiaryDark = Color(0xFFEF5350)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
