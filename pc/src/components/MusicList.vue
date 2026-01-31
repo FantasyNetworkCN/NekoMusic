f460d8d3f322590fd5d6a3ef963e3f73200c33c2
Nyanyagulugulu <qianxun114514@gmail.com>
2026-02-01 00:18:29 +0800

    添加播放列表

--- pc/src/components/MusicList.vue
+++ pc/src/components/MusicList.vue
@@ -73,6 +73,11 @@
                 <path fill="currentColor" d="M8 5v14l11-7z"/>
               </svg>
             </button>
+            <button class="action-btn" @click.stop="addToPlaylist(music)" title="添加到播放列表">
+              <svg viewBox="0 0 24 24" width="16" height="16">
+                <path fill="currentColor" d="M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z"/>
+              </svg>
+            </button>
             <button v-if="showFavorite" class="action-btn" @click.stop="toggleFavorite(music)" title="收藏">
               <svg viewBox="0 0 24 24" width="16" height="16">
                 <path :fill="isFavorite(music.id) ? '#e91e63' : 'currentColor'" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
@@ -152,6 +157,10 @@ const playMusic = (music) => {
   currentMusic.value = music
   localStorage.setItem('currentMusic', JSON.stringify(music))
   window.dispatchEvent(new CustomEvent('music-play', { detail: music }))
+}
+
+const addToPlaylist = (music) => {
+  window.dispatchEvent(new CustomEvent('add-to-playlist', { detail: music }))
 }
 
 const isFavorite = (musicId) => {
