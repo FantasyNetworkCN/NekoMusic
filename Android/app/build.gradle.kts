3df0f42b1cfbb60f896ea211eacbe3441a836d63
Nyanyagulugulu <qianxun114514@gmail.com>
2026-02-02 23:44:14 +0800

    升级

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.neko.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
+        versionCode = 30
         versionName = "20260202"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
