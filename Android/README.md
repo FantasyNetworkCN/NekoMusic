4edc85b5ec1d4e98a4529064b0ad6c3086dcc0fd
NyaNyagulugulu <support@cnmsb.xin>
2026-04-10 20:33:19 +0800

    refactor: architecture migration to submodules for pc and android

--- Android/README.md
+++
@@ -1,5 +0,0 @@
-# Neko云音乐安卓版
-![](https://count.getloli.com/get/@:NekoMusicAndroid?theme=moebooru)
-
-> [!TIP]
-> 🐾 **移动端入口**：[点击这里查看 Neko云音乐 安卓版仓库](https://github.com/MinecraftNekoServer/NekoMusicForPc)
--- Android/app/src/main/java/com/neko/music/data/database/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.neko.music.data.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [PlaylistEntity::class], version = 3, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun playlistDao(): PlaylistDao
-}
